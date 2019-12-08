a8a842e3eb10edb47d00e25118fc0ff51169c85f
Christian Rebischke <chris@nullday.de>
2019-12-08 22:29:36 +0100

    add util package
    Signed-off-by: Christian Rebischke <chris@nullday.de>

---
+++ util/util.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"fmt"
+	"time"
+)
+
+// Elapsed measures the execution time.
+// it can be used via defer util.Elapsed("functionname")
+// See also: https://stackoverflow.com/questions/45766572/is-there-an-efficient-way-to-calculate-execution-time-in-golang
+func Elapsed(what string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", what, time.Since(start))
+	}
+}
