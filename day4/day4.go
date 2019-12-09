105475cc8e4f5b12099b3b7a027c7ade2ea77e0e
Christian Rebischke <chris@nullday.de>
2019-12-09 23:11:09 +0100

    add solution for day4 part1
    Signed-off-by: Christian Rebischke <chris@nullday.de>

---
+++ day4/day4.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func checkNeighborship(n int) bool {
+	s := strconv.Itoa(n)
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == s[i+1] {
+			return true
+		}
+	}
+	return false
+}
+
+func checkNeverDecrease(n int) bool {
+	s := strconv.Itoa(n)
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] > s[i+1] {
+			return false
+		}
+	}
+	return true
+}
+
+// ruleOfTwo hehe.. I like this star wars metapher.
+func ruleOfTwo(n int) bool {
+	s := strconv.Itoa(n)
+	for _, e := range s {
+		var counter uint
+		for _, d := range s {
+			if e == d {
+				counter++
+			}
+			if counter > 2 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	// Contraints for this puzzle:
+	// It is a six-digit number.
+	// The value is within the range given in your puzzle input.
+	// Two adjacent digits are the same (like 22 in 122345).
+	// Going from left to right, the digits never decrease;
+	// they only ever increase or stay the same (like 111123 or 135679).
+
+	// first constraint: the passwords is in the range 183564-657474.
+	// this should also meet the constraint regarding 6 digits.
+	var result uint
+	for i := 183564; i <= 657474; i++ {
+		if checkNeighborship(i) {
+			if checkNeverDecrease(i) {
+				result++
+				// if ruleOfTwo(i) {
+				// 	result++
+				// }
+			}
+		}
+	}
+	fmt.Println(result)
+}
